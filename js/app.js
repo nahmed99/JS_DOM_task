25bf65d34dec5294b60b1d518ba957b39e3803c9
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-10 14:01:48 +0000

    Some code added to app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript has loaded');
+
+  const coolButton = document.querySelector('#button');
+  coolButton.addEventListener('click', handleOurButtonClick);
 });
+
+
+const handleOurButtonClick = function() {
+  console.log("The button has been clicked! 🧨")
+}
+
+
+/*
+
+  The Events
+  ==========
+
+  1. Element that dispatches the event
+  2. What event are we listening for?
+  3. The code to run when event is fired (addEventListener: callback function)
+
+  The Event Creators (corresponding numbers to above)
+  ===================================================
+
+  1. document
+  2. DOMContentLoaded
+  3. The function to run (in this case: arrow function)
+
+*/
